71ca6651a6ad57d6e332a2f6c01f1de0cc924c84
Daren Alfred <daren1997@hotmail.co.uk>
2017-04-11 20:54:43 +0100

    testing event handler(works nows)

--- Assets/Scripts/SpawnOnFloor.cs
+++ Assets/Scripts/SpawnOnFloor.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Academy.HoloToolkit.Unity;
+#if !UNITY_EDITOR && UNITY_METRO
+using System.Threading;
+using System.Threading.Tasks;
+#endif
 
 public class SpawnOnFloor : MonoBehaviour
 {
     Vector3 originalPosition;
     public Collider platformColl;
+    private Boolean planesDone;
     // Use this for initialization
     void Start()
     {
+        planesDone = false;
         // Grab the original local position of the sphere when the app starts.
         originalPosition = this.transform.localPosition;
         this.GetComponent<Renderer>().material.color = Color.red;
         platformColl = GetComponent<Collider>();
+        SurfaceMeshesToPlanes.Instance.MakePlanesComplete += StartGame;
     }
     private void Update()
     {
@@ -24,9 +33,9 @@ public class SpawnOnFloor : MonoBehaviour
             {
                 this.GetComponent<Renderer>().material.color = Color.green;
             }
-            else
+            else if(planesDone == true)
             {
-                this.GetComponent<Renderer>().material.color = Color.red;
+                this.GetComponent<Renderer>().material.color = Color.blue;
             }
         }
         
@@ -53,5 +62,9 @@ public class SpawnOnFloor : MonoBehaviour
     {
         // Just do the same logic as a Select gesture.
         OnSelect();
+    }
+    void StartGame(object source, EventArgs args)
+    {
+        planesDone = true;
     }
 }
