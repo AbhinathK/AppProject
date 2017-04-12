2c7aa39aed8508eeeb1abddf65adb83fba555941
Daren Alfred <daren1997@hotmail.co.uk>
2017-04-12 14:08:00 +0100

    code for settings buttons

--- Assets/Scripts/Settings/SPS/SetSpawn.cs
+++ Assets/Scripts/Settings/SPS/SetSpawn.cs
@@ -1,10 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
-using HoloToolkit.Unity;
 
 public class SetSpawn : MonoBehaviour
 {
+    public static Boolean platformIsSet { get; private set; }
+    public static Boolean circleIsSet { get; private set; }
+    public static Boolean lineIsSet { get; private set; }
+    public static float platformPos { get; private set; }
 
+    public static SetSpawn Instance { get; private set; }
+
+    void Start()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        platformIsSet = false;
+        circleIsSet = false;
+        lineIsSet = false;
+
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void OnGazeEnter()
     {
@@ -19,5 +42,19 @@ public class SetSpawn : MonoBehaviour
     public void OnSelect()
     {
         Application.LoadLevel("SetPlatformSpawn");
+    }
+
+    public void SetCircle(float x)
+    {
+        platformPos = x;
+        circleIsSet = true;
+
+    }
+
+    public void SetLine(float x)
+    {
+        platformPos = platformPos + x;
+        lineIsSet = true;
+
     }
 }
---
+++ Assets/Scripts/Settings/SetPatientSpawn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPatientSpawn : MonoBehaviour {
+
+    public static Boolean spawnIsSet { get; private set; }
+    public static Vector3 currentLoc { get; private set; }
+
+    public static SetPatientSpawn Instance { get; private set; }
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        spawnIsSet = false;
+
+        DontDestroyOnLoad(gameObject);
+		
+	}
+
+    public void OnGazeEnter()
+    {
+        GetComponent<Button>().OnPointerEnter(null);
+    }
+
+    public void OnGazeLeave()
+    {
+        GetComponent<Button>().OnPointerExit(null);
+    }
+
+    void OnSelect()
+    {
+        currentLoc = Camera.main.gameObject.transform.position;
+        spawnIsSet = true;
+    }
+}
