e5b28dd39db12eca1fc365939c992dc5e150c7d4
Daren Alfred <daren1997@hotmail.co.uk>
2017-04-12 14:53:47 +0100

    changes to delaytime button

--- Assets/Scripts/Settings/DelayTime.cs
+++ Assets/Scripts/Settings/DelayTime.cs
@@ -1,10 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using HoloToolkit.Unity;
 
 public class DelayTime : MonoBehaviour
 {
+    public static Boolean delayIsSet { get; private set; }
+    public static float delayTime { get; private set;  }
+    public static DelayTime Instance { get; private set; }
 
+    void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        delayIsSet = false;
+
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void OnGazeEnter()
     {
@@ -19,5 +37,12 @@ public class DelayTime : MonoBehaviour
     public void OnSelect()
     {
         Application.LoadLevel("SetDelayTime");
+    }
+
+    public void SetDelay(float x)
+    {
+        delayTime = x;
+        delayIsSet = true;
+
     }
 }
