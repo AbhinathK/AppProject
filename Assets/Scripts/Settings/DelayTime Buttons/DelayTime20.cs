c44b49799ea87ff65ff31210b7c8468f043c24bf
Daren Alfred <daren1997@hotmail.co.uk>
2017-04-12 15:01:16 +0100

    delaytime buttons

---
+++ Assets/Scripts/Settings/DelayTime Buttons/DelayTime20.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayTime20 : MonoBehaviour
+{
+
+    public void OnGazeEnter()
+    {
+        GetComponent<Button>().OnPointerEnter(null);
+    }
+
+    public void OnGazeLeave()
+    {
+        GetComponent<Button>().OnPointerExit(null);
+    }
+
+    public void OnSelect()
+    {
+        DelayTime.Instance.SetDelay(20F);
+    }
+}
