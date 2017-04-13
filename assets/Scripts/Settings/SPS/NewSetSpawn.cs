b84ac70e9e7d3a38dc377c6c54a23a843c3ed78d
AbhinathK <abhinathkumar@gmail.com>
2017-04-13 20:55:26 +0100

    Fixed buttons and camera positioning

---
+++ assets/Scripts/Settings/SPS/NewSetSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+
+public class NewSetSpawn : MonoBehaviour
+{
+
+
+    public void OnGazeEnter()
+    {
+        GetComponent<Button>().OnPointerEnter(null);
+    }
+
+    public void OnGazeLeave()
+    {
+        GetComponent<Button>().OnPointerExit(null);
+    }
+
+    public void OnSelect()
+    {
+        Application.LoadLevel("SetPlatformSpawn");
+    }
+}
