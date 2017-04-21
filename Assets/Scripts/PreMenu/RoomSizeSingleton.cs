5e0de116172b6f2b8fd5f21ddee71bfaab2c30d6
Daren Alfred <daren1997@hotmail.co.uk>
2017-04-21 10:32:13 +0100

    minor changes

--- Assets/Scripts/PreMenu/RoomSizeSingleton.cs
+++ Assets/Scripts/PreMenu/RoomSizeSingleton.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
 
 public class RoomSizeSingleton : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    public static Boolean sizeIsSet { get; private set; }
+    public static float roomSize { get; private set; }
+    public static RoomSizeSingleton Instance { get; private set; }
+
+    void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        roomSize = 3F;
+        sizeIsSet = false;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SetRoomSize(float x)
+    {
+        roomSize = x;
+        sizeIsSet = true;
+
+    }
 }
