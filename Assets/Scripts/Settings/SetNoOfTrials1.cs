50fd566e4733dc48a27510b636557ac1a801e655
AbhinathK <abhinathkumar@gmail.com>
2017-04-24 16:25:15 +0100

    tutorial/cursor fix

---
+++ Assets/Scripts/Settings/SetNoOfTrials1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+
+public class SetNoOfTrials1 : MonoBehaviour
+{
+
+
+    public void OnGazeEnter()
+    {
+        GetComponent<Button>().OnPointerEnter(null);
+    }
+
+    public void OnGazeLeave()
+    {
+        GetComponent<Button>().OnPointerExit(null);
+    }
+
+    public void OnSelect()
+    {
+        GlobalPositionTracker.Instance.UpdateRotation(Camera.main.transform.rotation);
+        GlobalPositionTracker.Instance.UpdatePos(Camera.main.transform.position);
+        Application.LoadLevel("SetNoTrials");
+    }
+
+
+}
